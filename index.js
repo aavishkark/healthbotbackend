9b97121b36b60d0f452c6b4376dccdfdeb3eaa56
Avishkar Kamble <aavishkark@gmail.com>
2025-05-07 17:07:01 +0530

    Add cors

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { connection } = require('./db');
 const { userRouter } = require('./Routes/users.route')
 const app=express();
+const cors = require('cors');
+app.use(cors());
 app.use(express.json())
 app.use('/',userRouter)
 app.listen(8080,async()=>{
