e767b2570058946aa884e3531a540b6538577dbe
Avishkar Kamble <aavishkark@gmail.com>
2025-05-08 17:25:19 +0530

    Fixed mongoose connection string issue

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-const connection = mongoose.connect(`${process.env.mongoose}`);
+const connection = mongoose.connect(`mongodb+srv://aavishkark:${process.env.MONGOOSE_PASSWORD}@cluster0.xh8xlzj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
 module.exports = {
     connection
 }
